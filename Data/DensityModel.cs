cac1d30019d11617074718b0850fc5ba7de5ccec
Dirk Weltz <130468140+DirkWeltz@users.noreply.github.com>
2023-10-16 22:28:28 +0200

    Added new density model entries

--- Data/DensityModel.cs
+++ Data/DensityModel.cs
@@ -1,9 +1,13 @@
-﻿namespace ZapClient.Data
+﻿using System;
+
+namespace ZapClient.Data
 {
     public class DensityModel
     {
-        public string UUID;
-        public string Name;
+        public string DensityModelUuid;
+        public string DensityModelName;
+        public DateTime DensityModelLastChangeDate;
+        public int DensityModelRevision;
         public DensityModelData[] Data;
     }
 }
